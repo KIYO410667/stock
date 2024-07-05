eb444f3fae36a97758257c619eeafdb0fb0330e9
D0987379 <hyou0218@gmail.com>
2024-07-06 05:32:47 +0800

    Add variables to Models - Stock.cs and Comment.cs

--- api/Models/Stock.cs
+++ api/Models/Stock.cs
@@ -10,6 +10,13 @@ namespace api.Models
         public int Id {get; set; }
         public string Symbol { get; set; } = string.Empty;
         public string CompanyName { get; set; } = string.Empty;
-        
+        public decimal Purchase { get; set; }
+        public decimal LastDiv { get; set; }
+        public string Industry { get; set; } = string.Empty;
+        public long MarketCap { get; set; }
+
+        public List<Comment> Comments { get; set; } = new List<Comment>();
+
+
     }
 }
