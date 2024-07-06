42b44ddd639accfe672e6d762927789ad6a30447
D0987379 <hyou0218@gmail.com>
2024-07-06 20:49:32 +0800

    To use EF and add migration

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f17b7ef4b93a5f64433ae54f8c7e5701994e3d07")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eb444f3fae36a97758257c619eeafdb0fb0330e9")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
