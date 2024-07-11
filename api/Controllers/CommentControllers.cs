be36d82bc92140df54b2bdd6a91d4630eaf9def5
D0987379 <hyou0218@gmail.com>
2024-07-11 11:01:27 +0800

    Comment System Building - Add CommentDto and CommentMappers for data transmit - Add ICommentRepository and CommentRepository for data calls in CommentController

---
+++ api/Controllers/CommentControllers.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using api.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using api.Mappers;
+
+namespace api.Controllers
+{
+    [Route("api/comment")]
+    [ApiController]
+    public class CommentControllers : ControllerBase
+    {
+        private readonly ICommentRepository _commentRepo;
+        public CommentControllers(ICommentRepository commentRepo)
+        {
+            _commentRepo = commentRepo;   
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var comment = await _commentRepo.GetAllAsync();
+
+            var commentDto = comment.Select(s => s.ToCommentDto());
+
+            return Ok(commentDto);
+        }
+    }
+}
